538b5ac456b858b05352e33ffa7bbd626ff985fe
vinicius <viniciuscoelhonau975@gmail.com>
2020-09-29 17:16:30 -0300

    init

---
+++ src/Commander/Dtos/CommandCreateDtos.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Commander.Dtos
+{
+    public class CommandCreateDto
+    {
+        [Required]
+        public string Line { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+    }
+}
---
+++ src/Commander/Migrations/20200718212154_Init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Commander.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Line = table.Column<string>(maxLength: 250, nullable: false),
+                    HowTo = table.Column<string>(nullable: false),
+                    Platform = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
